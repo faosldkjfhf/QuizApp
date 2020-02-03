b885c6c196d630af76ba871ab6d78047ee0be7d9
Kevin Liu <dethsandwich@gmail.com>
2020-02-03 15:50:59 -0500

    Monday Feb 3

--- app/src/main/java/com/liukevin/quizapp/MainActivity.java
+++ app/src/main/java/com/liukevin/quizapp/MainActivity.java
@@ -2,21 +2,30 @@ package com.liukevin.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button submitButton;
+    EditText responseText;
+    TextView displayText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         submitButton=findViewById(R.id.clickButton);
+        responseText=findViewById(R.id.responseEditText);
+        displayText=findViewById(R.id.textBox);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 System.out.println("Hello mom");
+                Log.i("testButton", "Hi dad! " + responseText.getText());
+                displayText.setText(displayText.getText() + " " + responseText.getText());
             }
         });
     }
